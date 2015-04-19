899487b91eeb6537b118d10e6d2c97aa977896ab
markmrose <markmrose@outlook.com>
2015-04-18 20:34:03 -0400

    t_area.py
    area of a triangle

---
+++ t_area.py
@@ -0,0 +1,23 @@
+# t_area.py
+# Written for Python 3.4.2
+# Revision date: 2/9/15
+
+# By: A. Programmer
+
+#---------------------------------------------------------------------------------------
+# This program finds the area of a triangle using the formula: area = 1/2*base*height
+# Values are assigned to base and height
+# Area is calculated and printed
+#---------------------------------------------------------------------------------------
+
+# assign the vlaue of 12 to base
+base = 12
+
+# assign the value of 16 to height
+height = 16
+
+# calculate area of a triangle using base and height
+area = 0.5*base*height
+
+# print the value of the area
+print ('The area of a triangle with a base of', base, 'inches and height of', height, 'inches is', area, 'square inches.')
